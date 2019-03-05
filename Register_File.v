65f67e25398945bb189fa06ffaa00b1ec4a82828
ASedaghati <ali.sedaghati@ut.ac.ir>
2019-03-05 10:38:48 +0330

    MIPS_PROCESSOR_ELEMENTS

---
+++ Register_File.v
@@ -0,0 +1,31 @@
+module Register_file ( clk , rst , Src1 , Src2 , Dest , Write_Val , Write_EN , Reg1 , Reg2 );
+	input clk , rst , Write_EN;
+	input [4:0] Src1 , Src2 , Dest;
+	input [31:0] Write_Val;
+	output reg [31:0] Reg1 , Reg2;
+
+	reg [31:0] Reg_data [0:31];
+
+	integer i;
+
+	assign Reg1 = Reg_data[Src1];
+	assign Reg2 = Reg_data[Src2];
+
+	always @ (negedge clk)
+	begin 
+		if ( rst )
+			begin
+				for ( i=0 ; i < 32 ; i++)
+					Reg_data [i] <= i;
+			end
+		else 
+			begin 
+				if ( Write_EN)
+					if (Dest != 5'b0)
+						Reg_data[Dest] = Write_Val;
+				
+			end
+	end
+
+
+
